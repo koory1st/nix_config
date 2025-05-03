de96566fe53d61d38544e8da96bdc84ab8ba429a
levy <32436334@qq.com>
2025-05-03 10:12:59 +0800

    update starship

--- nixos/home/zsh/default.nix
+++ nixos/home/zsh/default.nix
@@ -10,12 +10,15 @@
   # 启用 starship，这是一个漂亮的 shell 提示符
   programs.starship = {
     enable = true;
-    # 自定义配置
     settings = {
-      add_newline = false;
-      aws.disabled = true;
-      gcloud.disabled = true;
-      line_break.disabled = true;
+      add_newline = true;
+      command_timeout = 1300;
+      scan_timeout = 50;
+      format = "$all$nix_shell$nodejs$lua$golang$rust$php$git_branch$git_commit$git_state$git_status\n$username$hostname$directory";
+      character = {
+        success_symbol = "[](bold green) ";
+        error_symbol = "[✗](bold red) ";
+      };
     };
   };
 
