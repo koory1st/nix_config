2c9c8b380c0656fc2e9e0e27e816f62e2e0278f6
levy <32436334@qq.com>
2025-05-03 09:35:16 +0800

    fix tmux error

--- nixos/home/terminal/default.nix
+++ nixos/home/terminal/default.nix
@@ -7,9 +7,12 @@
 
   programs.tmux = {
     enable = true;
-    clock24 = true;
-    extraConfig = '' # used for less common options, intelligently combines if defined in multiple places.
-      ...
-    '';
+    # clock24 = true;
+    # extraConfig = '' # used for less common options, intelligently combines if defined in multiple places.
+    #   ...
+    # '';
   };
+
+  #plugins: https://search.nixos.org/packages?channel=24.11&from=0&size=50&sort=relevance&type=packages&query=tmuxPlugins
+
 }
