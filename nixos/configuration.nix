4420343de8bfeb26136a7404845f0b32a750da81
levy <32436334@qq.com>
2025-05-03 09:35:26 +0800

    add nerd

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -83,6 +83,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    nerdfonts
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
